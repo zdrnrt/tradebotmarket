fd9bf28225d9fa4503c850d1cf190d592bf11eb8
Задорин Артем <artfan95@gmail.com>
2025-02-15 21:38:53 +0700

    Верстка шапки

--- src/shared/header/ui/Header.jsx
+++ src/shared/header/ui/Header.jsx
@@ -1,8 +1,16 @@
+import style from './Header.module.scss'
+
 
 export default function Header(){
   return (
-    <header>
-      <button></button>
+    <header className={style['header']}>
+      <button className={`${style['header__control']} ${style['header__control--menu']}`}>
+        <img src="/img/header/menu.png" alt="menu" />
+      </button>
+      <span className={style['header__title']}>Dashboard</span>
+      <button className={`${style['header__control']} ${style['header__control--refresh']}`}>
+        <img src="/img/header/refresh.png" alt="Refresh" />
+      </button>
     </header>
   )
 }
