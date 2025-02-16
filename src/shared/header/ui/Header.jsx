2de50187f00b9eaf2d50347dde31252ffb479f93
Задорин Артем <artfan95@gmail.com>
2025-02-16 11:56:57 +0700

    Правки шапки и подвала для мобильной версии

--- src/shared/header/ui/Header.jsx
+++ src/shared/header/ui/Header.jsx
@@ -4,11 +4,11 @@ import style from './Header.module.scss'
 export default function Header(){
   return (
     <header className={style['header']}>
-      <button className={`${style['header__control']} ${style['header__control--menu']}`}>
+      <button className={style['header__control']}>
         <img src="/img/header/menu.png" alt="menu" />
       </button>
       <span className={style['header__title']}>Dashboard</span>
-      <button className={`${style['header__control']} ${style['header__control--refresh']}`}>
+      <button className={style['header__control']}>
         <img src="/img/header/refresh.png" alt="Refresh" />
       </button>
     </header>
